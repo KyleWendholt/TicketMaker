8018fcbd71e05f64fda2e7027253672b9aaa1356
Kyle Wendholt <kgwen1@gmail.com>
2023-05-07 17:26:49 -0400

    added Ticket post endpoint to make new tickets

--- server/controllers/tickets.js
+++ server/controllers/tickets.js
@@ -14,6 +14,32 @@ app.get("/", (req, res) => {
       .getTicket(req.params.id)
       .then((x) => res.status(200).send(x))
       .catch(next);
-  })
+})
+.post("/", (req, res, next) => {
+  const auth = authToken(req.body.JWTtoken);
+  if (typeof auth === "string") {
+    res.status(400).send(auth);
+    return;
+  }
+  const ticket = {
+    owner_id: auth.userid,
+    creationTime: new Date(),
+    title: req.body.title,
+    status: req.body.status,
+    content: req.body.content,
+  };
+  console.log(ticket);
+  tickets
+    .addTicket(ticket)
+    .then((result) => {
+      if (result.insertedId){
+        res.status(201).send(result.insertedId);
+      }
+      else {
+        res.status(400).send(result);
+      }
+    })
+    .catch(next);
+})
 
 module.exports = app;
