a8140ff764f6586c19251c076d918c5aa3e6edb6
Kyle Wendholt <kgwen1@gmail.com>
2023-07-09 14:19:03 -0400

    added endpoints for token refresh and for register and logout

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express.Router();
 const auth = require("../models/auth.js");
+const users = require("../models/users.js");
 
 
 app.post("/login", (req, res, next) => {
@@ -11,6 +12,7 @@ app.post("/login", (req, res, next) => {
           res.status(401).send("Invalid username or password");
           return;
         }
+        console.log(user);
         const accessToken = auth.generateAccessToken(user);
         const refreshToken = auth.generateRefreshToken(user);
         res.status(200).setHeader('set-cookie', `refreshToken=${refreshToken}; Path=/; HttpOnly; Max-Age=${auth.refreshTokenLife}; SameSite=None; Secure`)
@@ -18,5 +20,30 @@ app.post("/login", (req, res, next) => {
       })
     .catch(next);
 })
+.post("/refresh",auth.authRefreshToken, (req, res, next) => {
+  const accessToken = auth.generateAccessToken(req.user);
+  res.status(200).send({ accessToken });
+})
+.post("/logout", (req, res, next) => {
+  res.status(200).setHeader('set-cookie', `refreshToken=; Path=/; HttpOnly; Max-Age=0; SameSite=None; Secure`)
+  .send();
+})
+.post("/register", (req, res, next) => {
+  const user = {
+    username: req.body.username,
+    password: req.body.password,
+    role: req.body.role,
+    profilePicture: req.body.profilePicture,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+  };
+  users
+    .createUser(user)
+    .then(() => {
+      res.status(201).send();
+    })
+    .catch(next);
+});
 
 module.exports = app;
