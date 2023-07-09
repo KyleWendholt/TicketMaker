33424577aba42ff75ab6a6464f7b55f372f41879
Kyle Wendholt <kgwen1@gmail.com>
2023-07-09 02:20:14 -0400

    style changes to navbar, added home route

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,74 +1,67 @@
 <template>
-  <nav class="navbar is-fixed-top secondary-color" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-    </a>
-
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-      <a class="navbar-item">
-        Home
+  <nav
+    class="navbar is-fixed-top secondary-color"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="" width="112" height="28" />
       </a>
 
-      <a class="navbar-item">
-        Documentation
+      <a
+        :class="{ 'is-active': isActive }"
+        @click="isActive = !isActive"
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
       </a>
+    </div>
 
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
-        </a>
+    <div
+      id="navbarContent"
+      class="navbar-menu"
+      :class="{ 'is-active': isMenuActive }"
+    >
+      <div class="navbar-start">
+        <router-link to="/" class="navbar-item"> Home </router-link>
 
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            About
-          </a>
-          <a class="navbar-item">
-            Jobs
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a>
+        <router-link class="navbar-item" to="/profile"> Profile </router-link>
+          <router-link
+            v-if="session.user?.isAdmin"
+            class="navbar-item"
+            to="/admin"
+          >
+            Admin
+          </router-link>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light"> Log in </a>
+          </div>
         </div>
       </div>
     </div>
-
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">
-            Log in
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { RouterLink } from 'vue-router';
+import { ref } from "vue";
+import { RouterLink } from "vue-router";
 import session from "../stores/session";
 
-
+let isMenuActive = ref(false);
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
