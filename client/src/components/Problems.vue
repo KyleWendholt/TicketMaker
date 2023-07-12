a7abcaf1e44525bb9ed3a07172bcbb2fedec211d
Kyle Wendholt <kgwen1@gmail.com>
2023-07-11 20:54:59 -0400

    now calls reAuth when 403 is returned. will be reimplimented in other components and views.

--- client/src/components/Problems.vue
+++ client/src/components/Problems.vue
@@ -8,23 +8,29 @@
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue';
-import TicketComponent from './Ticket.vue'
-import { Ticket } from '../stores/tickets';
-import { getProblemTickets } from '../stores/tickets';
+import { reactive } from "vue";
+import TicketComponent from "./Ticket.vue";
+import { Ticket } from "../stores/tickets";
+import { getProblemTickets } from "../stores/tickets";
+import session from "../stores/session";
+import { reAuthenticate } from "../stores/session";
 
 const problemTickets = reactive([] as Ticket[]);
 updateTickets();
 
 function updateTickets() {
   getProblemTickets().then((tickets) => {
-    if (tickets.list){
-    problemTickets.splice(0, problemTickets.length, ...tickets.list);
-  }
+    if (session.error && session.error.status === 403) {
+      if (reAuthenticate()) {
+        console.log("reauthenticated");
+        updateTickets();
+      }
+    }
+    if (tickets.list) {
+      problemTickets.splice(0, problemTickets.length, ...tickets.list);
+    }
   });
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
