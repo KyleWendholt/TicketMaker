11c21cad924d3644adaa14d601c215a5f73ae8f8
Kyle Wendholt <kgwen1@gmail.com>
2023-07-11 20:53:06 -0400

    reworked api and myFetch calls, and added reAuthenticate function session.error now has status refresh token is now not exported and is async logout is now async, and makes call to auth/logout to clear refresh cookie. also redirects to login  screen and clears session user and error messages set error updated to include status and clear error was created to remove null set error calls

--- client/src/stores/session.ts
+++ client/src/stores/session.ts
@@ -1,3 +1,4 @@
+import router from "../router";
 import myFetch from "../services/myFetch";
 import type { User } from "./users";
 
@@ -6,47 +7,79 @@ import { computed, reactive } from "vue";
 const session = reactive({
   user: null as User | null,
   loading: 0,
-  error: null as string | null,
+  error: null as { message: string; status?: number } | null,
   messages: [] as Message[],
   token: null as string | null,
 });
 
 export function login(username: string, password: string) {
-  return api<{accessToken:string}>("auth/login", { username, password }, "POST");
+  return api<{ accessToken: string }>(
+    "auth/login",
+    { username, password },
+    "POST"
+  );
 }
 
 export function register(user: User) {
-  return api<{accessToken:string}>("auth/register", { user }, "POST");
+  return api<{ accessToken: string }>("auth/register", { user }, "POST");
 }
 
-export function refreshToken(){
-  return api<{accessToken:string}>("auth/refresh", null, "POST");
+async function refreshToken() {
+  return api<{ accessToken: string }>("auth/refresh", null, "POST");
 }
 
-export function logout() {
+export async function logout() {
+  clearError();
   session.user = null;
+  session.token = null;
+  router.push("/login");
+  return api("auth/logout", null, "POST");
 }
 
 export async function api<T>(url: string, data: any = null, method?: string) {
   session.loading++;
-  setError(null);
+  clearError();
   try {
     return await myFetch<T>(url, data, method);
   } catch (error: any) {
-    setError(error.message ?? (error as string));
+    setError(error.message ?? (error as string), error.status);
   } finally {
     session.loading--;
   }
   return {} as T;
 }
 
+export function reAuthenticate() {
+  console.log("reauthenticating");
+  refreshToken().then((res) => {
+    console.log(res);
+    if (res.accessToken) {
+      session.token = res.accessToken;
+      console.log("reauthenticated");
+      clearError();
+      return true;
+    } else {
+      console.log("reauthentication failed");
+      //const error = session.error;
+      logout().then(() => {
+        return false;
+      });
+    }
+  });
+  return false;
+}
+
 export const isLoading = computed(() => !!session.loading);
 
-export function setError(error: string | null) {
-  session.error = error;
-  if (error) {
-    session.messages.push({ type: "danger", text: error });
+export function setError(message: string, status?: number) {
+  if (status) {
+    session.error = { message, status };
   }
+  session.messages.push({ type: "danger", text: message });
+}
+
+export function clearError() {
+  session.error = null;
 }
 export interface Message {
   text: string;
