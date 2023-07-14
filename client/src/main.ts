b5d9d76294db12a08f4f76a913992f543b980713
KyleWendholt <101617205+KyleWendholt@users.noreply.github.com>
2023-07-13 23:02:07 -0400

    switched which icon was imported

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,11 +6,11 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faTicket } from '@fortawesome/free-solid-svg-icons';
 
 library.add(
   faEnvelope,
-  faHome,
+  faTicket,
 );
 
 
