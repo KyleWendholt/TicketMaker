59513adda84289cc7aeb4308003813f72ebb63af
KyleWendholt <101617205+KyleWendholt@users.noreply.github.com>
2023-07-16 22:02:10 -0400

    basic user display. needs better prettiness

--- client/src/components/User.vue
+++ client/src/components/User.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-
+  <div class="columns">
+    <div class="column">{{ user.email }}</div>
+    <div class="column">{{ user.firstname }} {{ user.lastname }}</div>
+    <div class="column">{{ user.roles }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { defineProps } from "vue";
+import { User } from "../stores/users";
+
+defineProps<{
+  user: User;
+}>();
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
