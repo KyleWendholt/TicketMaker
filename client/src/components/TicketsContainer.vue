1ba2044ff16d8b788996f8011684ce9a9c27b5b3
Kyle Wendholt <kgwen1@gmail.com>
2023-07-24 15:51:45 -0400

    dont work yet, start of tabs

--- client/src/components/TicketsContainer.vue
+++ client/src/components/TicketsContainer.vue
@@ -3,7 +3,7 @@
     <div class="panel-heading">
       <div class="has-text-centered level">
         <p class="level-item">{{ title }}</p>
-        <a  @click="$emit('refresh')" class="icon">
+        <a @click="$emit('refresh')" class="icon">
           <font-awesome-icon :icon="['fas', 'arrows-rotate']"
         /></a>
       </div>
@@ -16,6 +16,12 @@
         <div class="column">Created</div>
       </div>
     </div>
+    <p class="panel-tabs" v-if="needsTabs">
+      <a v-for="tab in tabs" @click="selectTab(tab)">
+        {{ tab }}
+      </a>
+      
+    </p>
     <TicketComponent
       v-for="ticket in tickets"
       :ticket="ticket"
@@ -28,14 +34,31 @@
 <script setup lang="ts">
 import TicketComponent from "./Ticket.vue";
 import { Ticket } from "../stores/tickets";
+import { defineProps, defineEmits, ref, } from "vue";
 
 defineEmits(["refresh"]);
 
-defineProps<{
+const props = defineProps<{
   title: string;
   tickets: Ticket[];
   showRequester?: boolean;
 }>();
+
+const needsTabs = props.tickets.length > 5;
+console.log(props.tickets.length);
+
+
+const tabs = ref(
+  props.tickets.length % 5 === 0
+    ? Array.from(Array(props.tickets.length / 5).keys())
+    : Array.from(Array(Math.floor(props.tickets.length / 5) + 1).keys())
+);
+
+let visibleTickets = ref(props.tickets.slice(0, 5));
+
+function selectTab(tab: number) {
+  visibleTickets.value = props.tickets.slice(tab-1 * 5, tab-1 * 5 + 5);
+}
 </script>
 
 <style scoped>
